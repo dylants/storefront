5faa71a1b56d0c573cddda95dd864f14ffb250f9
Dylan Smith <dylantsmith@gmail.com>
2013-04-21 22:21:37 -0500

    switch to new bonsai environment variable

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -3,7 +3,7 @@ if ENV['BONSAI_INDEX_URL']
   Tire.configure do
     url "http://index.bonsai.io"
   end
-  ELASTICSEARCH_INDEX_NAME = ENV['BONSAI_INDEX_URL'][/[^\/]+$/]
+  ELASTICSEARCH_INDEX_NAME = ENV['BONSAI_URL'][/[^\/]+$/]
 else
   app_name = Rails.application.class.parent_name.underscore.dasherize
   ELASTICSEARCH_INDEX_NAME = "#{app_name}-#{Rails.env}"
